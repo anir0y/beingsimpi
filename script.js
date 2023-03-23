3b9afdb277fd3aa27e5f0f7663924c3c9c19e225
beast-machine <animeshroy@live.com>
2023-03-24 04:28:46 +0530

    added some more functions

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log(`%c
+    ♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥
+    ♥                      ♥
+    ♥          simpi       ♥
+    ♥                      ♥
+    ♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥
+`, 'color: red; font-size: 20px;');
+
+console.log(`%c
+made by @anir0y
+`, 'color: green; font-size: 9px;');
